d86ee585996fc18db5aa95c1f387da846310ca28
unknown <justdanielfloyd@gmail.com>
2015-09-09 10:59:31 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="es"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>&copy; Disservicios - Global</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/apple-touch-icon.png">
+        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/animate.css">
+        <script src="<?php bloginfo('template_url'); ?>/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+
+    </head>
+
+<body>
+ 
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->    
+    <nav class="navbar navbar-inverse navbar-fixed-top nav_1" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="celu" href="#">Teléfono: 01 8000 000 000</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse navbar-right">
+           <ul class="nav navbar-nav redes_social">
+              <li><i class="fa fa-facebook-square"></i></li>
+              <li><i class="fa fa-twitter-square"></i></li>
+              <li><i class="fa fa-instagram"></i></li>
+            </ul>
+        </div><!--/.navbar-collapse-->
+      </div>
+    </nav>
+
+
+    
+      <div class="encabezado">
+       <div class="_head">
+       <div class="_logo"><img src="<?php bloginfo('template_url'); ?>/img/logo.png" alt=""></div>
+        </div>
+         
+      </div>
+    
+ 
+
+  <nav class="navbar navbar-inverse navbar-fixed-top nav_2" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse _nav">
+           <ul class="nav navbar-nav ">
+              <li><a href="#">Inicio</a></li>
+              <li><a href="#">Nosotros</a></li>
+              <li><a href="#">Servicios</a></li>
+              <li><a href="#">Contacto</a></li>
+            </ul>
+        </div><!--/.navbar-collapse-->
+      </div>
+    </nav>
+    <div class="slider">
+    <?php echo do_shortcode("[huge_it_videogallery id='2']"); ?>
+    </div>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+   <!-- <div class="jumbotron">
+      <div class="container">
+      <!--slider-
+        <h1 class="wow bounceInUp">Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+      </div>
+    </div>-->
+
+   <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4 wow bounceInUp" data-wow-delay="400ms">
+          <div class="edif1">
+            <img src="<?php bloginfo('template_url'); ?>/img/edificio.jpg" alt="">
+          </div>
+          <h2>Título Número 1</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        </div>
+        <div class="col-md-4 wow bounceInUp" data-wow-delay="500ms">
+          <div class="edif1">
+          <img src="<?php bloginfo('template_url'); ?>/img/edificio.jpg" alt="">
+          </div>
+          <h2>Título Número 2</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+       </div>
+        <div class="col-md-4 wow bounceInUp" data-wow-delay="600ms">
+          <div class="edif1">
+              <img src="<?php bloginfo('template_url'); ?>/img/edificio.jpg" alt="">
+          </div>
+          <h2>Título Número 3</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          </div>
+
+          
+          <div class="col-md-8 wow bounceInUp" data-wow-delay="700ms" style="margin-top: 50px; ">
+            <h2>Contenido Adicional</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        <div class="col-md-4 wow bounceInUp" data-wow-delay="800ms" style="margin-top: 50px;">
+          <div class="edif1">
+              <img src="<?php bloginfo('template_url'); ?>/img/edificio.jpg" alt="">
+        </div>
+        <h2>Título Número 4</h2>
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          </div>
+        </div>
+      </div>
+    
+       <div class="_footer">
+         <div class="imgfoot">
+             <footer>
+             <div class="col-md-12 text-center menut">
+             <ul>
+               <li><h4><a href="#">Home</a></h4></li>
+               <li><h4><a href="#">Nosotros</a></h4></li>
+               <li><h4><a href="#">Servicios</a></h4></li>
+               <li><h4><a href="#">Contacto</a></h4></li>
+             </ul>
+           </div>
+           <div class="col-md-12 text-center text_down"><h4>&copy; DisServicios - Reservados todos los derechos 2015 - Desarrollado por Empresa <h4></div>
+           </footer>
+         </div>
+       </div>
+      
+    </div> 
+
+</body></script></head></html>
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/vendor/bootstrap.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/wow.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+
+
+        </script>
+    </body>
+</html>
